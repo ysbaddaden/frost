bec43236bfe0a6a2cce390c964e69f0568a7d5bf
Julien Portalier <julien@portalier.com>
2015-11-23 16:42:16 +0100

    Handlers: LogHandler + PublicFileHandler

---
+++ src/server/handlers/log_handler.cr
@@ -0,0 +1,29 @@
+require "http/server"
+
+module Trail::Server
+  class LogHandler < HTTP::Handler
+    def call(request)
+      time = Time.now
+      call_next(request).tap do |response|
+        elapsed = elapsed_text(Time.now - time)
+        Trail.logger.info "#{ request.method } #{ request.path.inspect } #{ response.status_code } (#{ elapsed })"
+      end
+    end
+
+    private def elapsed_text(elapsed)
+      if (minutes = elapsed.total_minutes) >= 1
+        return "#{ minutes.round(2) }m"
+      end
+
+      if (seconds = elapsed.total_seconds) >= 1
+        return "#{ seconds.round(2) }s"
+      end
+
+      if (millis = elapsed.total_milliseconds) >= 1
+        return "#{ millis.round(2) }ms"
+      end
+
+      "#{ (millis * 1000).round(2) }µs"
+    end
+  end
+end
---
+++ src/server/handlers/public_file_handler.cr
@@ -0,0 +1,46 @@
+require "http/server"
+require "openssl/digest"
+
+module Trail::Server
+  class PublicFileHandler < HTTP::Handler
+    # TODO: set cache-control headers (would need digests)
+    # OPTIMIZE: cache & serve already deflated files
+    # OPTIMIZE: memoize etags
+
+    def initialize(@publicdir)
+    end
+
+    def call(request)
+      path = @publicdir + request.path
+      return call_next(request) unless public_file?(path)
+
+      contents = File.read(path)
+      etag = digest(contents)
+
+      headers = HTTP::Headers{
+        "Content-Type": mime_type(path),
+        "Etag": etag,
+      }
+
+      if request.headers["If-None-Match"]? == etag
+        HTTP::Response.new(304, nil, headers)
+      else
+        HTTP::Response.new(200, contents, headers)
+      end
+    end
+
+    private def public_file?(path)
+      File.exists?(path) && !File.directory?(path)
+    end
+
+    private def mime_type(path)
+      Trail::Support::Mime.mime_type(File.extname(path))
+    end
+
+    private def digest(contents)
+      digest = OpenSSL::Digest.new("MD5")
+      digest.update(contents)
+      digest.hexdigest
+    end
+  end
+end

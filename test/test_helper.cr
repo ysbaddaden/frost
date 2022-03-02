dc9c7b49c88fa3eff8017a934ad11c2eb42aeeb5
Julien Portalier <julien@portalier.com>
2022-03-02 16:18:05 +0100

    Fix: init the MIME library before running tests

--- test/test_helper.cr
+++ test/test_helper.cr
@@ -1,6 +1,9 @@
 require "minitest/autorun"
 require "../src/frost"
 
+# initialize the mimetype library to avoid lazy loading it on-demand (MT unsafe)
+MIME.init(true)
+
 module Frost
   def self.clear_routes
     @@handler = nil

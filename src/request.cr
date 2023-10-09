5fe6e28e7c48f945db6dcb17fd2d0cded3647af6
Julien Portalier <julien@portalier.com>
2023-10-09 11:28:55 +0200

    Add Frost::Request#accept?(mime)

--- src/request.cr
+++ src/request.cr
@@ -6,6 +6,8 @@ struct Frost::Request
   def initialize(@request : HTTP::Request)
   end
 
+  # Returns true if the request originates from a XMLHttpRequest call. You'll
+  # have to set the `x-requested-with: xmlhttprequest` header yourself.
   def xhr? : Bool
     if x_requested_with = @request.headers["x-requested-with"]?
       "xmlhttprequest".compare(x_requested_with, case_insensitive: true) == 0
@@ -14,24 +16,52 @@ struct Frost::Request
     end
   end
 
+  # Returns the content-type of the request's body.
   def content_type : String?
     @request.headers["content-type"]?
   end
 
-  # Returns true if the request's content type is JSON (i.e. `application/json`)
+  # Returns true if the request's body content type is JSON (i.e. `application/json`)
   # or a derivate (e.g. `application/geo+json`).
   def json? : Bool
-    content_type =~ %r{^application/(?:\w+\+|)json}
+    !!(content_type =~ %r{^application/(?:\w+\+|)json})
   end
 
-  # Returns true if the request's content type is
-  # `application/x-www-form-urlencoded`.
+  # Returns true if the request's body content type is an urlencoded form
+  # (`application/x-www-form-urlencoded`).
   def urlencoded? : Bool
     !!content_type.try(&.starts_with?("application/x-www-form-urlencoded"))
   end
 
-  # Returns true if the request's content type is `multipart/form-data`.
+  # Returns true if the request's body content type is a multipart form
+  # (`multipart/form-data`).
   def multipart? : Bool
     !!content_type.try(&.starts_with?("multipart/form-data"))
+  end
+
+  # Returns true if the request's `accept` header includes the given mimetype.
+  #
+  # For example: `request.accept?("text/event-stream")`
+  def accept?(mime : String) : Bool
+    if header = @request.headers["accept"]?
+      return true if header == mime
+
+      header.split(',') do |accept|
+        accept = accept.strip
+
+        return true if accept == mime
+        return true if accept.includes?("*/*")
+
+        if index = accept.index("/*")
+          return true if mime.to_slice[0..index] == accept.to_slice[0..index]
+        end
+
+        if index = accept.index(';')
+          return true if mime.to_slice == accept.to_slice[0...index]
+        end
+      end
+    end
+
+    false
   end
 end
---
+++ test/request_test.cr
@@ -0,0 +1,89 @@
+require "./test_helper"
+
+class Frost::RequestTest < Minitest::Test
+  def test_xhr?
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers.new))
+    refute request.xhr?
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "x-requested-with" => "something-else",
+    }))
+    refute request.xhr?
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "x-requested-with" => "xmlhttprequest",
+    }))
+    assert request.xhr?
+  end
+
+  def test_content_type?
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers.new))
+    assert_nil request.content_type
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "content-type" => "application/json;charset=utf-8",
+    }))
+    assert_equal "application/json;charset=utf-8", request.content_type
+  end
+
+  def test_json?
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers.new))
+    refute request.json?
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "content-type" => "application/json;charset=utf-8",
+    }))
+    assert request.json?
+  end
+
+  def test_urlencoded?
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers.new))
+    refute request.urlencoded?
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "content-type" => "application/x-www-form-urlencoded",
+    }))
+    assert request.urlencoded?
+  end
+
+  def test_urlencoded?
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers.new))
+    refute request.multipart?
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "content-type" => "multipart/form-data",
+    }))
+    assert request.multipart?
+  end
+
+  def test_accept?
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "accept" => "*/*",
+    }))
+    assert request.accept?("text/event-stream")
+    assert request.accept?("text/plain")
+    assert request.accept?("image/jpeg")
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "accept" => "text/event-stream",
+    }))
+    assert request.accept?("text/event-stream")
+    refute request.accept?("text/plain")
+    refute request.accept?("image/jpeg")
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "accept" => "text/*",
+    }))
+    assert request.accept?("text/event-stream")
+    assert request.accept?("text/plain")
+    refute request.accept?("text-x/other")
+    refute request.accept?("image/jpeg")
+
+    request = Request.new(HTTP::Request.new("GET", "/", HTTP::Headers{
+      "accept" => "text/html , application/xhtml+xml, application/xml;q=0.9",
+    }))
+    assert request.accept?("text/html")
+    assert request.accept?("application/xhtml+xml")
+    assert request.accept?("application/xml")
+  end
+end
